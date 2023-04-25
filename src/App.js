5f37523b590d2bc5a1dcece2fe23579aa0c6d4a3
sohailnasir74atWork <sohailnasir74business@gmail.com>
2023-04-25 20:15:16 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Home from './HomePage/Index';
-// import AboutUs from './StaticPages/AboutUs';
+import AboutUs from './StaticPages/AboutUs';
 import Footer from './Common/Footer';
 import Header from './Common/Header';
-// import PrivacyPolicy from './StaticPages/PrivacyPolicy';
-// import ContactUs from './StaticPages/ContactUs';
+import PrivacyPolicy from './StaticPages/PrivacyPolicy';
+import ContactUs from './StaticPages/ContactUs';
 import React, {lazy, Suspense} from 'react';
 import CircularColor from './Common/Loader';
 
-const PrivacyPolicy = lazy(()=>import("./StaticPages/PrivacyPolicy"))
-const ContactUs = lazy(()=>import("./StaticPages/ContactUs"))
-const AboutUs = lazy(()=>import("./StaticPages/AboutUs"))
+// const PrivacyPolicy = lazy(()=>import("./StaticPages/PrivacyPolicy"))
+// const ContactUs = lazy(()=>import("./StaticPages/ContactUs"))
+// const AboutUs = lazy(()=>import("./StaticPages/AboutUs"))
 // const Footer = lazy(()=> import("./Common/Footer"))
 const renderLoader = () => <><CircularColor/></>
 
@@ -21,16 +21,12 @@ function App() {
     <BrowserRouter>
         <div className="App" id="3000">
           <Header />
-          <Suspense>
-          <Routes>
+            <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about-us' element={<AboutUs />} />
             <Route path='/privacy-policy' element={<PrivacyPolicy />} />
             <Route path='/contact-us' element={<ContactUs />} />
           </Routes>
-          </Suspense>
-
-
           <Footer />
         </div>
     </BrowserRouter>
