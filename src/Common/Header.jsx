7c4106ac66c2f4d4da8a6e828af306773a00bb0f
sohailnasir74atWork <sohailnasir74business@gmail.com>
2023-04-25 13:14:49 +0500

    commmit

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -1,116 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from "../logo1.svg"
-// import HeaderSidebar from './HeaderSideBar'
+import HeaderSidebar from './HeaderSideBar'
 import "../../src/App.css"
 import { useLockBodyScroll } from './ScrollCustomHook'
 import "./Header.css"
-// export const Sidebar = styled(Box)({
-//   position: 'fixed',
-//   display: 'flex',
-//   width: '100%',
-//   height: '100vh',
-//   transition: "all 0.5s ease !important",
-//   top: 0,
-//   left: 0,
-//   right: 0,
-//   bottom: 0,
-//   zIndex: 5,
-//   cursor: 'pointer',
-//   overFlow: "hidden",
-//   "& .overlay": {
-//     position: 'fixed',
-//     width: '100%',
-//     height: '100vh',
-//     top: 0,
-//     left: 0,
-//     right: 0,
-//     bottom: 0,
-//     overFlow: "hidden",
-//     backgroundColor: "rgba(0, 0, 0, 0.8)",
-//     zIndex: 5,
-//     '& nav': {
-//       fontFamily: "var(--text-font)",
-//       fontSize: '18px !important',
-//       fontWeight: '600 !important',
-//       width: '85%',
-//       height: '100%',
-//       background: "white",
-//       right: "0",
-//       position: "absolute",
-//       '& .top-div': {
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         textAlign: 'center',
-//         background: "rgba(0, 0, 0, 0.8)",
-//         height: '100px',
-//         width: '100%',
-//        },
-//       '& .sidebar-footer': {
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         textAlign: 'center',
-//         height: '100px',
-//         position: "absolute",
-//         width: '100%',
-//         borderTop: '3px solid #A8A8A8',
-//         borderBottom: '3px solid #A8A8A8',
-//       },
-//       '& ul': {
-//         height: '100%',
-//         width: '100%',
-//         listStyle: 'none',
-//         padding: '0px',
-//         marginTop: "0px",
-//         '& li': {
-//           minHeight: '60px',
-//           display: 'flex',
-//           alignItems: 'center',
-//           justifyContent: 'flex-start',
-//           padding: '10px 12px',
-
-//           '& a': {
-//             position: 'relative',
-//             color: "var(--primary-color)",
-//             fontFamily: "var(--text-font)",
-//             textDecoration: 'none',
-//             fontSize: '1rem',
-//             fontWeight: '700',
-//             display: 'block',
-//             width: '100%',
-//             textAlign: "left",
-//           },
-//             '& a.click span::before': {
-//             content: '"\f068"',
-//           },
-//         },
-//         '& li:hover': {
-//           background: 'rgba(0, 0, 0, .1)',
-
-//         },
-//       },
-//     },
-
-//     "& .closebtn": {
-//       border: "none",
-//       background: "#36454F",
-//       height: "40px",
-//       width: "15%",
-//       padding: "10px auto",
-//       position: "absolute",
-//       right: "85%",
-//       "& i": {
-//         fontSize: "20px",
-//         textStroke: '1px black',
-//       }
-//     }
-//   }
-// })
-
-
-
+import bars from "../bars.png"
 const Header = () => {
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -135,8 +30,8 @@ const Header = () => {
   useLockBodyScroll(isSidebarOpen)
   return (
     <div className='wrapper-header'>
-        {/* <div id="slide" style={{ transform: "translateX(100%)" }}>
-          {isSidebarOpen && <HeaderSidebar isOpen={isSidebarOpen} close={closeSidebar} />}</div> */}
+        <div id="slide" style={{ transform: "translateX(100%)" }}>
+          {isSidebarOpen && <HeaderSidebar isOpen={isSidebarOpen} close={closeSidebar} />}</div>
         <div className="grid-container">
           <div className="grid-item">
            
@@ -192,10 +87,11 @@ const Header = () => {
                 </Link>
               </div>
             </div>
-            <div className='bars'>
-              {/* <MenuIcon style={{ color: "white" }} className='icon' onClick={openSidebar}/> */}
-            </div>
+            
           </div>
+          <div className='bars'>
+          <img src={bars} alt='Menu' onClick={openSidebar}/>
+            </div>
         </div>
     </div>
   )
--- src/Common/HeaderSideBar.jsx
+++ src/Common/HeaderSideBar.jsx
@@ -1,52 +1,51 @@
-// import React from 'react'
-// import {Link} from "react-router-dom";
-// import CloseIcon from '@mui/icons-material/Close';
-// import { Sidebar } from './Header';
-// import logo2 from "../logo1.svg";
-
-
-// const sidebarItem = [
-//      {
-//       text: "Home",
-//       path: "/"
-//     },
-//     {
-//       text: 'About Us',
-//       path: '/about-us'
-//     },
-//     {
-//       text: 'Contact Us',
-//       path: '/contact-us'
-//     },
-//     {
-//       text: 'Privacy Policy',
-//       path: '/privacy-policy'
-//     },
-//   ]
-//     const HeaderSidebar = ({ close }) => {
-//     return (
-//       <Sidebar id="sidebarMenu" >
-//         <div className="overlay">
-//           <nav onClick={(event) => event.stopPropagation()}>
-//             <div className='top-div'><img src={logo2} alt="logo" style={{height:"50px"}}/></div>
-//             <ul>
-//               {sidebarItem.map(item => {
-//                 return (
-//                   <li>
-//                     <Link to={item.path} onClick={close} >
-//                         <span>{item.text}</span>
-//                     </Link>
-//                   </li>
-//                 )
-//               })}
-//             </ul>    
-//           </nav>
-//           <div className="closebtn" onClick={close}>
-//             <CloseIcon style={{fontSize:"40px", color:"White"}}/>
-//           </div>
-//         </div>
-//       </Sidebar>
-//     );
-//   };
+import React from 'react'
+import {Link} from "react-router-dom";
+import { Sidebar } from './Header';
+import logo2 from "../logo1.svg";
+import "./Header.css"
+import closeButton from "../close.png"
+const sidebarItem = [
+     {
+      text: "Home",
+      path: "/"
+    },
+    {
+      text: 'About Us',
+      path: '/about-us'
+    },
+    {
+      text: 'Contact Us',
+      path: '/contact-us'
+    },
+    {
+      text: 'Privacy Policy',
+      path: '/privacy-policy'
+    },
+  ]
+    const HeaderSidebar = ({ close }) => {
+    return (
+      <div className='sidebar' id="sidebarMenu" >
+        <div className="overlay" onClick={close}>
+          <nav onClick={(event) => event.stopPropagation()}>
+            <div className='top-div'><img src={logo2} alt="logo" style={{height:"50px"}}/></div>
+            <ul>
+              {sidebarItem.map(item => {
+                return (
+                  <li>
+                    <Link to={item.path} onClick={close} >
+                        <span>{item.text}</span>
+                    </Link>
+                  </li>
+                )})}
+            </ul>  
+            <div className="closebtn" onClick={close}>
+            <img src={closeButton} alt='close'/>
+          </div>  
+          </nav>
+         
+        </div>
+      </div>
+    );
+  };
    
-// export default HeaderSidebar
+export default HeaderSidebar
